3def1d9a49e160a17ba6f7b64d2b75215ef5b122
dkeruza <njdemichelis@gmail.com>
2023-11-11 16:24:10 -0500

    Added game detection

--- src/lindbergh/config.c
+++ src/lindbergh/config.c
@@ -70,6 +70,24 @@ static int detectGame()
         return 0;
     }
 
+    if (strstr(program_invocation_name, "abc1080"))
+    {
+        config.game = ABC;
+        return 0;
+    }
+
+    if (strstr(program_invocation_name, "drive"))
+    {
+        config.game = SRTV;
+        return 0;
+    }
+    
+        if (strstr(program_invocation_name, "dsr"))
+    {
+        config.game = RTUNED;
+        return 0;
+    }
+    
     config.game = UNKNOWN;
     return 1;
 }
